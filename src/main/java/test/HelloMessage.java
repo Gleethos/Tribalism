58bbb8642678a9f24fc9a001ab497e18a88a6e64
Gleethos <danneydan@gmx.at>
2023-01-16 22:06:16 +0100

    WIP bootstrap setup

---
+++ src/main/java/test/HelloMessage.java
@@ -0,0 +1,22 @@
+package test;
+
+public class HelloMessage {
+
+    String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
