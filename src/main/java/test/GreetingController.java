58060b0adf3b2918ce12f874c871eac5993ce95c
Gleethos <Gleethos@gmx.at>
2023-01-17 21:29:01 +0100

    fix some stuff

--- src/main/java/test/GreetingController.java
+++ src/main/java/test/GreetingController.java
@@ -47,10 +47,7 @@ public class GreetingController {
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         this.bindingWebSocket.onMessage(message.getName());
-<<<<<<< HEAD
         UI.sync();
-=======
->>>>>>> origin/main
         return new Greeting(this.sent[0]);
         //return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
