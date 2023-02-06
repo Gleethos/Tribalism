1782c9be85a514d386cceed58e95bcd9f36deb80
Michael Schulz <mi.schulz@hotmail.com>
2023-02-06 23:18:00 +0100

    Add button and input component

---
+++ src/main/web-portal/src/components/atoms/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Button({
+  kind,
+  buttonStyles,
+  onClick,
+  icon,
+  children,
+}) {
+  const classes =
+    kind === 'primary'
+      ? 'bg-gray-800 shadow-sm hover:shadow-lg hover:bg-gray-800/80 hover:scale-105 transition duration-300 text-white'
+      : 'bg-gray-500';
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${buttonStyles} px-4 py-3 rounded-lg ${classes}`}
+    >
+      {children}
+    </button>
+  );
+}
