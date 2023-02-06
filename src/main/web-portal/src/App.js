522c6f33a1019fb5f8a0df0e3684880c8a2efec1
Michael Schulz <mi.schulz@hotmail.com>
2023-02-06 17:32:38 +0100

    Fix body tag issue

--- src/main/web-portal/src/App.js
+++ src/main/web-portal/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { Var, Val, VM, Session, connect } from "./mvvm/backend-binder";
+import { Var, Val, VM, Session, connect } from './mvvm/backend-binder';
 import LoginView from './starter/LoginView.js';
 import * as ReactDOM from 'react-dom';
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
   const [content, setContent] = useState(null);
@@ -11,46 +11,44 @@ function App() {
     'ws://localhost:8080/websocket',
     'app.ContentViewModel-0', // The "main" view model where the application starts
     (session, contentVM) => {
-
-      console.log("Current view model: " + contentVM);
+      console.log('Current view model: ' + contentVM);
       // Relevant fields:
       const clazz = contentVM.class;
       const state = contentVM.state;
       const props = state.props; // The properties of the view model
       const methods = state.methods; // The methods of the view model
 
-      contentVM.content().get( vm => {
-
-        console.log("Received content page: " + vm.class);
+      contentVM.content().get((vm) => {
+        console.log('Received content page: ' + vm.class);
 
         // Now let's check if the class is a login page
-        if ( vm.class === 'app.LoginViewModel' ) {
-            // We set the content from the LoginView
-            setContent(<LoginView vm={vm}/>);
-        } else if ( vm.class === 'app.RegisterViewModel' ) {
-            // TODO: Implement the register page
-            // We make the main page empty:
-            setContent(<div>THIS IS WIP</div>);
+        if (vm.class === 'app.LoginViewModel') {
+          // We set the content from the LoginView
+          setContent(<LoginView vm={vm} />);
+        } else if (vm.class === 'app.RegisterViewModel') {
+          // TODO: Implement the register page
+          // We make the main page empty:
+          setContent(<div>THIS IS WIP</div>);
         }
-      })
+      });
 
       // If the user does not want to login, we can switch to the register page using the switch button
-      const switchButton = document.getElementById("switch-id");
+      const switchButton = document.getElementById('switch-id');
       //switchButton.onclick = () => contentVM.showRegister();
-    });
-    return (
-        <div className="App">
-            <header className="App-header">
-                <title>Tribee Login!</title>
-                <button id="switch-id">Switch</button>
-            </header>
-            <body>
-            <div id="main-content" className="App-body">
-                {content}
-            </div>
-            </body>
-        </div>
-    );
+    },
+  );
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <title>Tribee Login!</title>
+        <button id='switch-id'>Switch</button>
+      </header>
+
+      <div id='main-content' className='App-body'>
+        {content}
+      </div>
+    </div>
+  );
 }
 
 export default App;
