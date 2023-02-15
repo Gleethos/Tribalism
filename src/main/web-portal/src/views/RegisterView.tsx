6eb06825303c8d60d81d9f15e5a344fb03a15141
Michael Schulz <mi.schulz@hotmail.com>
2023-02-15 18:45:46 +0100

    Implement suggested changes

--- src/main/web-portal/src/views/RegisterView.tsx
+++ src/main/web-portal/src/views/RegisterView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GiRaiseZombie } from 'react-icons/gi';
+import { GiRaiseZombie, GiShamblingZombie } from 'react-icons/gi';
 import Button from '../components/atoms/Button';
 import Input from '../components/atoms/Input';
 import useProperty from '../hooks/useProperty';
@@ -28,7 +28,7 @@ export default function RegisterView({ vm }: any) {
         <h1 className='text-2xl text-gray-700 p-2 font-bold  border-b-2 border-gray-400 pb-4 mb-4'>
           Register, if you dare ...
         </h1>
-        <div className='login__username'>
+        <div className='register__username'>
           <Input
             inputStyles={'opacity-50 hover:opacity-100'}
             labelStyles={'font-bold text-lg text-gray-800'}
@@ -39,7 +39,7 @@ export default function RegisterView({ vm }: any) {
             onChange={(event) => setUsername(event.target.value as string)}
           />
         </div>
-        <div className='login__password'>
+        <div className='register__password'>
           <Input
             inputStyles={'opacity-50 hover:opacity-100'}
             labelStyles={'font-bold text-lg text-gray-800'}
@@ -50,18 +50,29 @@ export default function RegisterView({ vm }: any) {
             onChange={(event) => setPassword(event.target.value)}
           />
         </div>
-
-        <Button
-          buttonStyles='flex gap-3 mt-6 items-center justify-center'
-          kind='primary'
-          onClick={() => vm.register()}
-        >
-          <span className='block'>Register</span>
-          <GiRaiseZombie className='text-2xl' />
-        </Button>
+        <div className='flex flex-col gap-4'>
+          <Button
+            buttonStyles='flex gap-3 mt-6 items-center justify-center'
+            kind='primary'
+            onClick={() => vm.register()}
+          >
+            <span className='block'>Register</span>
+            <GiRaiseZombie className='text-2xl' />
+          </Button>
+          <span>Already have an account?</span>
+          <Button
+            buttonStyles='flex gap-3 scale-[0.9] !bg-gray-500/80 hover:!scale-[1]'
+            kind='secondary'
+          >
+            <span className='block' onClick={() => vm.switchToLogin()}>
+              Login
+            </span>
+            <GiShamblingZombie className='text-2xl' />
+          </Button>
+        </div>
         <div>
           <span
-            id={'login-feedback'}
+            id={'register-feedback'}
             className={`block pt-3 text-${feedbackColor}-900`}
           >
             {feedback as React.ReactNode}
